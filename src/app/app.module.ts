35808ec099c4608a85a2f47f8714e10cb2fe63e6
DESKTOP-UOSJ1LD\Guilherme <a14012@bcc.unifal-mg.edu.br>
2022-03-15 20:02:50 -0300

    Arrumando aos e finalizando section sobre nós

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SobreNosComponent } from './pages/home-page/sobre-nos/sobre-nos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomePageComponent
+    HomePageComponent,
+    SobreNosComponent
   ],
   imports: [
     BrowserModule,
